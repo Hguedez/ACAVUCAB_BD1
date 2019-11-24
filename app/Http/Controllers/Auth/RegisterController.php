6950007428362ec51a0fd9cfce3c292e3a60ce57
Hguedez <57779032+Hguedez@users.noreply.github.com>
2019-11-24 19:41:23 -0400

    Update RegisterController.php
    Registrar con usuarios actualizados

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -52,6 +52,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            
         ]);
     }
 
@@ -67,6 +68,11 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'name2' => $data['name2'],
+            'apellido' => $data['apellido'],
+            'apellido2' => $data['apellido2'],
+            'cedula' => $data['cedula'],
+            'rif' => $data['rif'],
         ]);
     }
 }
